4e5a0b051ee9601aa790e62c98509bfd27594050
anand madhav <madhava36@gmail.com>
2017-11-29 10:32:58 +0530

    Initial Commit

---
+++ WiproAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WiproAssignment
+//
+//  Created by anand madhav on 29/11/17.
+//  Copyright © 2017 anand madhav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WiproAssignment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WiproAssignment
+//
+//  Created by anand madhav on 29/11/17.
+//  Copyright © 2017 anand madhav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
